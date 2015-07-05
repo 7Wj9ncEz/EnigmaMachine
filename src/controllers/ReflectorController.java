bdfc794ef87238ce74a11dbe1cd7bb98e0fedc01
Lucas Amoêdo <lucas_advc@outlook.com>
2015-07-05 11:32:36 -0300

    Create alphabet interface

--- src/controllers/ReflectorController.java
+++ src/controllers/ReflectorController.java
@@ -1,13 +1,14 @@
 package controllers;
 
+import interfaces.Alphabet;
 import models.Reflector;
 
-public class ReflectorController extends Reflector{
+public class ReflectorController extends Reflector implements Alphabet{
 
 	public char reflect (char letter){
 		int i, position = 0;
-		for (i = 0; i < getAlphabet().length(); i++){
-			if (letter == getAlphabet().charAt(i)){
+		for (i = 0; i < ALPHABET.length(); i++){
+			if (letter == ALPHABET.charAt(i)){
 				position = i;
 				break;
 			}
