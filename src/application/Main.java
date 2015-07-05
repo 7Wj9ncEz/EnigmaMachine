3f673b49466cf0fb8418f101de62072345def2dc
Lucas Amoêdo <lucas_advc@outlook.com>
2015-07-05 11:16:26 -0300

    Create main class

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Scanner;
+
+import controllers.ReflectorController;
+import models.*;
+
+
+public class Main {
+	public static void main (String[] args){
+		Rotor rotors = new Rotor();
+		ReflectorController reflector = new ReflectorController();
+		
+	}
+}
