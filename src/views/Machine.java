8da13df7adfe969865bcef2b37513b31000e918d
Lucas Amoêdo <lucas_advc@outlook.com>
2015-07-05 23:18:28 -0300

    Create virtual rotors panel

--- src/views/Machine.java
+++ src/views/Machine.java
@@ -8,14 +8,16 @@ import javax.swing.*;
 public class Machine extends JFrame{
 	Lampboard lampboard = new Lampboard();
 	Keyboard keyboard = new Keyboard();
+	RotorsPanel rotorsPanel = new RotorsPanel();
 	
 	public Machine(){
 		setTitle("Enigma Machine");
 		setSize(450,400);
 		setBackground(Color.white);
 		setVisible(true);
-		setLayout(new GridLayout(2, 1));
+		setLayout(new GridLayout(3, 1));
 		
+		add(rotorsPanel.getRotorsPanel());
 		add(lampboard.getLampboard());
 		add(keyboard.getKeyboard());
 		
